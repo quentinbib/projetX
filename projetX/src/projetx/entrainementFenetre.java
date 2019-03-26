516f8f94516480c2658a734e3623c86da0fce5c1
Hugo <Hugo@DESKTOP-4DA123G>
2019-03-26 21:26:06 +0100

    nouvelle page : fenetre entrainement

--- projetX/src/projetx/entrainementFenetre.java
+++ projetX/src/projetx/entrainementFenetre.java
@@ -4,7 +4,21 @@
  * and open the template in the editor.
  */
 package projetx;
-
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
 /**
  *
  * @author BERNARD
