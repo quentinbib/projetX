355cc19ce76c1f88aaf644151e5d87b23799e9ba
BERNARD <BERNARD@BERNARD-PC>
2019-04-20 13:17:05 +0200

    conteur bug

--- projetX/src/projetx/noobFenetre.java
+++ projetX/src/projetx/noobFenetre.java
@@ -40,7 +40,8 @@ public class noobFenetre extends JFrame implements ActionListener
         String ligneFichier = fR.readLine();
         return ligneFichier;
     }
-    
+    int count = 0;
+    Font font1 = new Font("", Font.PLAIN, 25);
     private JButton retour;
     private JButton next;
     private JButton valider;
@@ -51,14 +52,14 @@ public class noobFenetre extends JFrame implements ActionListener
     static String normal[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"," "};
      public void actionPerformed(ActionEvent ev)
      {
-         
+        
         if(ev.getSource() == valider)
         {
+            
             String nouvotexte = ligne;
             String motEnMorse = "" ;
             int lettre = 0; 
             String stringvalueof;
-            
             for (int i = 0; i < nouvotexte.length(); i++) 
             {
                 stringvalueof = String.valueOf(nouvotexte.charAt(lettre));
@@ -73,6 +74,7 @@ public class noobFenetre extends JFrame implements ActionListener
             }
         if (morse2.getText().equalsIgnoreCase(motEnMorse))
         {
+            count = count + 1;
             try 
             {
                noobFenetre fenetre = new noobFenetre();
@@ -84,10 +86,8 @@ public class noobFenetre extends JFrame implements ActionListener
         }
         else
         {
-            System.out.println(motEnMorse);
-            Font font1 = new Font("", Font.PLAIN, 14);
             morse2.setText("reesayer");
-            morse2.setForeground(Color.red);
+            morse2.setForeground(Color.black);
             morse2.setFont(font1);
         }
         }
@@ -118,6 +118,7 @@ public class noobFenetre extends JFrame implements ActionListener
      
 public noobFenetre() throws FileNotFoundException, IOException
 {
+    
     //On selectionne le fichier qu'on va utiliser
     fR = new BufferedReader(new FileReader(new File("src/levels/niveaunoob.txt")));
     Dimension ecran = Toolkit.getDefaultToolkit().getScreenSize();
@@ -153,8 +154,8 @@ public noobFenetre() throws FileNotFoundException, IOException
         morse2 = new JTextField();
         morse2.setBounds((int)(width/2-width*0.40/2),(int)(height*0.40), (int)(width*0.40), (int)(height*0.06));
         morse2.setHorizontalAlignment(JTextField.CENTER);
-        Font font1 = new Font("", Font.PLAIN, 25);
         morse2.setForeground(Color.black);
+        morse2.setFont(font1);
         add(morse2);
         
         next = new JButton ("Passer");
@@ -171,6 +172,50 @@ public noobFenetre() throws FileNotFoundException, IOException
         valider.setBounds((int)(width/2-width*0.11/2),(int)(height*0.52), (int)(width*0.11),(int)(height*0.06));
         contenant.add(valider);
         valider.addActionListener(this);
+        
+        Font font2 = new Font("", Font.PLAIN, 18);
+        JLabel AToM = new JLabel();
+        AToM.setBounds((int)(width*0.06),(int)(height*0.02), (int)(width*0.055), (int)(height*0.9));
+        AToM.setText("<html>A<br/><br/>B<br/><br/>C<br/><br/>D<br/><br/>E<br/><br/>F<br/><br/>G<br/><br/>H<br/><br/>I<br/><br/>J<br/><br/>K<br/><br/>L<br/><br/>M</html>");
+        AToM.setFont(font2); 
+        contenant.add(AToM);
+        
+        JLabel MorseAToM = new JLabel();
+        MorseAToM.setBounds((int)(width*0.09),(int)(height*0.02), (int)(width*0.055), (int)(height*0.9));
+        MorseAToM.setText("<html>.-<br/><br/>-...<br/><br/>-.-.<br/><br/>-..<br/><br/>.<br/><br/>..-.<br/><br/>--.<br/><br/>....<br/><br/>..<br/><br/>.---<br/><br/>-.-<br/><br/>.-..<br/><br/>--</html>");
+        MorseAToM.setFont(font2);
+        contenant.add(MorseAToM);
+        
+        JLabel NToZ = new JLabel();
+        NToZ.setBounds((int)(width*0.15),(int)(height*0.02), (int)(width*0.055), (int)(height*0.9));
+        NToZ.setText("<html>N<br/><br/>O<br/><br/>P<br/><br/>Q<br/><br/>R<br/><br/>S<br/><br/>T<br/><br/>U<br/><br/>V<br/><br/>W<br/><br/>X<br/><br/>Y<br/><br/>Z</html>");
+        NToZ.setFont(font2);
+        contenant.add(NToZ);
+        
+        JLabel MorseNToZ = new JLabel();
+        MorseNToZ.setBounds((int)(width*0.18),(int)(height*0.02), (int)(width*0.055), (int)(height*0.9));
+        MorseNToZ.setText("<html>-.<br/><br/>---<br/><br/>.--.<br/><br/>--.-<br/><br/>.-.<br/><br/>...<br/><br/>-<br/><br/>..-<br/><br/>...-<br/><br/>.--<br/><br/>-..-<br/><br/>-.--<br/><br/><--..</html>");
+        MorseNToZ.setFont(font2); 
+        contenant.add(MorseNToZ);
+        
+        JLabel chiffre = new JLabel();
+        chiffre.setBounds((int)(width*0.83),(int)(height*0.02), (int)(width*0.055), (int)(height*0.8));
+        chiffre.setText("<html>0<br/><br/>1<br/><br/>2<br/><br/>3<br/><br/>4<br/><br/>5<br/><br/>6<br/><br/>7<br/><br/>8<br/><br/>9</html>");
+        chiffre.setFont(font2);
+        contenant.add(chiffre);
+        
+        JLabel MorseChiffre = new JLabel();
+        MorseChiffre.setBounds((int)(width*0.86),(int)(height*0.02), (int)(width*0.055), (int)(height*0.8));
+        MorseChiffre.setText("<html>-----<br/><br/>.----.<br/><br/>..---<br/><br/>...--<br/><br/>....-<br/><br/>.....<br/><br/>-....<br/><br/>--...<br/><br/>---..<br/><br/>----.</html>");
+        MorseChiffre.setFont(font2); 
+        contenant.add(MorseChiffre);
+        
+        System.out.println(count);
+        JLabel vousAvez = new JLabel();
+        vousAvez.setBounds((int)(width*0.42),(int)(height*0.72),(int)(width*0.6),(int)(height*0.06));
+        vousAvez.setText("Vous avez réussi " + (count) +  " niveaux.");
+        vousAvez.setFont(font2); 
+        contenant.add(vousAvez);
 }
 }
 
